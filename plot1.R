482b156490ff91198379a9ca1f80ac773746eb04
AmirHex <amir@cs.toronto.edu>
2014-12-04 09:35:54 -0500

    plot1

--- plot1.R
+++ plot1.R
@@ -1,2 +1,10 @@
-#reading file #
-hp <- readcsv(file="household_power_consumption.txt", sep=",")
+# clear console
+cat("\014")  
+#reading file
+hp <- read.csv(file="household_power_consumption.txt", sep=";")
+hp <- transform(hp, Global_active_power = as.numeric(levels(Global_active_power))[Global_active_power], Date = as.Date(Date, "%d/%m/%Y"))
+hp <- subset(hp, Date >= as.Date("2007-02-01"))
+hp <- subset(hp, Date <= as.Date("2007-02-02"))
+png(file ="plot1.png") 
+hist(hp$Global_active_power, col="red", main="Global Active Power", xlab="Global Active Power (kilowatts)")
+dev.off()
